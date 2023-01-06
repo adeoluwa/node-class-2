4af08bef1e12e1825418e4e6ddf797a458bac2f2
Deolu_Josef <kiishijoseph@gmail.com>
2023-01-06 16:35:29 +0100

    express js class

---
+++ client/server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+
+import skiTerms from './ski-terms.json' assert { type: 'json' };
+
+import bodyParser from 'body-parser';
+
+import fs from 'fs';
+
+const app = express();
+
+app.use('/', express.static('./client'));
+
+app.get('/dictionary', (req, res) => {
+  res.json(skiTerms);
+});
+
+app.post('/dictionary', bodyParser.json(), (req, res) => {
+  skiTerms.push(req.body);
+  res.json({
+    status: 'Success',
+    term: req.body,
+  });
+});
+
+app.delete('/dictionary/:term', (req, res) => {
+  skiTerms.filter(({ term }) => term !== req.params.term);
+  save();
+  res.json({
+    status: 'success',
+    removed: req.params.term,
+    newLength: skiTerms.length,
+  });
+});
+
+const save = () => {
+  fs.writeFile(
+    './data/ski-terms.json',
+    JSON.stringify(skiTerms, null, 2),
+    (err) => {
+      if (err) {
+        throw err;
+      }
+    }
+  );
+};
+
+app.listen(3000, () => {
+  console.log('ski dictionary running at 3000');
+});
